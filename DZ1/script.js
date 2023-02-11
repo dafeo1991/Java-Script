b07c0f8e4ff9ae59a395971950cbe30359a8e36d
darya <feoktistova.darya2017@yandex.ru>
2023-02-11 12:28:03 +0700

    new

---
+++ DZ1/script.js
@@ -0,0 +1,10 @@
+// Задача 1
+let temp = 30;
+let far = (9 / 5) * temp + 32; 
+console.log(`Температура по Цельсию ${temp} градусов.
+Температура по Фаренгейту ${far} градусов.`);
+
+// Задача 2
+let myName = "Darya";
+let admin = myName; 
+console.log(admin); 
---
+++ seminar1/script.js
@@ -0,0 +1,46 @@
+// Задача 1
+let y = "Текст из Script.js"
+console.log(y);
+
+// Задача 2
+let num = 123;
+console.log(num);
+
+let a = 5;
+a = 7;
+console.log(a);
+
+let b = 1+2+3;
+console.log(b);
+
+// Задача 3
+let c = 10;
+let d = 2;
+
+let sum = c+d;
+let dif = c-d;
+let mult = c*d;
+let div = c/d;
+console.log(sum, dif, mult, div);
+console.log(sum+dif+mult+div);
+
+// Задача 5
+let s = 1.5;
+let h = 0.75;
+console.log(s+h);
+
+let g = -5;
+console.log(-g)
+
+// Задача 6
+// Способы склеивания и вывода на экран
+
+let firstName = "Darya";
+let lastName = "Feoktistova";
+console.log(`${firstName} ${lastName}`);
+
+let r = "java";
+let o = "script";
+console.log(r+o);
+console.log(r, o);
+console.log("Hello world");
